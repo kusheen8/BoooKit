7d37b7101dd4c61fff5334ce52c61a3125b5601c
kusheen8 <kusheendhar942@gmail.com>
2025-10-31 10:47:15 +0530

    changes

--- server/vite.ts
+++ server/vite.ts
@@ -81,7 +81,11 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+ app.get("*", (req, res, next) => {
+    // Skip API routes
+    if (req.path.startsWith("/api")) {
+      return next();
+    }
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
